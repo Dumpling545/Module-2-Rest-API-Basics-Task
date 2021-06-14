81c02768c65218834d5cc802687d67afb8ead900
Dumpling545 <y.kardziyaka@gmail.com>
2021-06-14 20:28:02 +0300

    removed aspect

--- service/src/main/java/com/epam/esm/service/config/ServiceConfig.java
+++ service/src/main/java/com/epam/esm/service/config/ServiceConfig.java
@@ -2,11 +2,9 @@ package com.epam.esm.service.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"com.epam.esm.service"})
 @PropertySource("classpath:validation.properties")
 @PropertySource("classpath:exception.properties")
