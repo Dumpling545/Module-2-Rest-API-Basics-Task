fc8b87dd37f1387e2bcc3015f038a853dd708984
Yan_Kardziyaka <y.kardziyaka@gmail.com>
2021-08-13 19:31:35 +0300

    configuration changes for deploying war

--- config/src/main/java/com/epam/esm/GiftCertificateSystemApplication.java
+++ config/src/main/java/com/epam/esm/GiftCertificateSystemApplication.java
@@ -2,7 +2,9 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,7 +15,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 @ConfigurationPropertiesScan
-public class GiftCertificateSystemApplication {
+public class GiftCertificateSystemApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GiftCertificateSystemApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GiftCertificateSystemApplication.class, args);
 	}
